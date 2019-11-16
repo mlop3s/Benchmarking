8644552ddfcfe31717b0e6e84f3631b2857ec58c
Marco Lopes <mlopes_filho@hotmail.com>
2019-11-16 22:33:56 +0100

    Add project files.

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarking
+{
+    public class DateTimeBenchmark
+    {
+        [Params(1000, 10000)]
+        public int N;
+
+        [Benchmark]
+        public void DateTimeNow()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                var now = DateTime.Now;
+            }
+        }
+
+        [Benchmark]
+        public void DateTimeUtcNow()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                var now = DateTime.Now;
+            }
+        }
+
+        [Benchmark]
+        public void TimeStamp()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                var now = System.Diagnostics.Stopwatch.GetTimestamp();
+            }
+        }
+    }
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<DateTimeBenchmark>();
+        }
+    }
+}
